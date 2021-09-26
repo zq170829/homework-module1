331744c7550e2e7261425b26ea7d0bc167d2b42d
Qi Zhang <zq170829@gmail.com>
2021-09-25 20:33:15 -0500

    w4d2 homework

--- js/randomcodes.js
+++ js/randomcodes.js
@@ -1,9 +1,12 @@
+//create variables to store generated codes and the type of characters we want to show as codes
+var code = " "; // initialize to null value
+var getCode = " ";
+var btnvalue;
+var str =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&*";
+
 //Function to generate combination of characters
 function generateCode() {
-  //create variables to store generated codes and the type of characters we want to show as codes
-  var code = " "; // initialize to null value
-  var str =
-    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&*";
   //generate character multiple times using a loop
   for (i = 1; i <= 8; i++) {
     var char = Math.random() * str.length; //random select a character from the variable
@@ -16,9 +19,29 @@ function generateCode() {
 document.getElementById("codes").innerHTML = generateCode();
 
 //Disable Button
-function disableButton() {
-  document.getElementById("submit").disabled = true;
+function disableButton(btnvalue) {
+  document.getElementById("submit").disabled = btnvalue; 
+  if (btnvalue == true) {
+    document.getElementById("submit").style.backgroundColor =
+      "rgba(73, 119, 209, 0.3)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 0.5)";
+  } else {
+    document.getElementById("submit").style.backgroundColor =
+      "rgba(73, 119, 209, 1)";
+    document.getElementById("submit").style.color = "rgba(255, 255, 255, 1)";
+  }
 }
+disableButton(btnvalue);
 
-//Activate function
-disableButton();
+//listen to user input code
+var codebox = document.getElementById("codeentered");
+codebox.addEventListener("input", evaluateCode);
+function evaluateCode() {
+  getCode = document.getElementById("codeentered").value;
+  var charset1 = getCode.trim();
+  var charset2 = code.trim();
+  if (charset1.length == charset2.length && charset1 == charset2) {
+    disableButton(false);
+  }
+}
+evaluateCode();

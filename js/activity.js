fe2a3f172d66113fbce2af4a4b4f315e7f3255b4
Qi Zhang <zq170829@gmail.com>
2021-10-06 00:23:33 -0700

    w5d3 homework

--- js/activity.js
+++ js/activity.js
@@ -1,7 +1,30 @@
-$(document).ready(function () {
-  $("td")
-    .not(':contains("Not Available")')
-    .click(function () {
-      $(this).toggleClass("highlight");
-    });
+// $(document).ready(function () {
+//   $("td")
+//     .not(':contains("Not Available")')
+//     .click(function () {
+//       $(this).toggleClass("highlight");
+//     });
+// });
+
+$(document).ready(function(){
+  $('td').click(function(){
+      var content = $(this).text();
+      var colIndex = $(this).index();
+      var colTitle = $('th:eq(' + colIndex + ')').text();
+      if (content !="Not Available"){
+          $(this).toggleClass('highlight');
+
+      if($(this).hasClass('highlight')){
+          $('#displaySelected').css('visibility','visible');
+          $('#result').append('<p>'+ content+' at '+ colTitle +'</p>');
+      } else{
+          $('#result p:contains('+content+')').remove();
+
+          if($('#result').has('p').length == false) {
+              $('#displaySelected').css('visiblity','hidden');
+              $('#displaySelected').css('margin-top','0');
+          }
+      }
+  }
+  });
 });
